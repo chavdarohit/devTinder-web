b6fcf3db4b41296f6237f2a4c18d4984ca3258a1
Rohit Chavda <rohitchavda@socialpilot.co>
2025-12-11 19:01:49 +0530

    login api call and axios

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,49 @@
+import { useState } from "react";
+import axios from "axios";
 const Login = () => {
-  return <div>This is Login Page</div>;
+  const [email, setEmail] = useState("rohit@gmail.com");
+  const [password, setPassword] = useState("Rohit@123");
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/login",
+        {
+          email,
+          password
+        },
+        { withCredentials: true }
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  return (
+    <div className="flex justify-center my-10">
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <label className="label">Email</label>
+        <input
+          type="email"
+          className="input"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label className="label">Password</label>
+        <input
+          type="password"
+          className="input"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="btn btn-neutral mt-4" onClick={handleLogin}>
+          Login
+        </button>
+      </fieldset>
+    </div>
+  );
 };
 
 export default Login;
